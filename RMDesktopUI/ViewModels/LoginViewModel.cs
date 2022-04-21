de9d177a21a892fe715d2eacf36d21b1455de48f
basaif <iman.basaif@gmail.com>
2022-04-21 16:51:17 +0300

    Wire up login form to Api
    Connects the login button to api authenication endpoint (\token) gets back the bearer token or throws exception if it fails.

--- RMDesktopUI/ViewModels/LoginViewModel.cs
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using RMDesktopUI.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,14 @@ namespace RMDesktopUI.ViewModels
         private string _userName;
 
         private string _password;
+
+        private IAPIHelper _apiHelper;
+
+        public LoginViewModel(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
         public string UserName
         {
             get { return _userName; }
@@ -50,9 +59,17 @@ namespace RMDesktopUI.ViewModels
             }
         }
 
-        public void LogIn()
+        public async Task LogIn()
         {
-           
+            try
+            {
+                var result = await _apiHelper.Authenticate(UserName, Password);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message); 
+            }
         }
     }
 }
