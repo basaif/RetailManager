74df0098ee97ceaaa5c29cdc1f847cc43ed9bc63
basaif <iman.basaif@gmail.com>
2022-05-09 18:27:05 +0300

    Add Api project with .NET Core 3

---
+++ RMApi/Controllers/InventoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RMApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        [Authorize(Roles = "Manager,Admin")]
+        public List<InventoryModel> Get()
+        {
+            InventoryData data = new InventoryData();
+            return data.GetInventory();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public void Post(InventoryModel item)
+        {
+            InventoryData data = new InventoryData();
+
+            data.SaveInventoryRecord(item);
+        }
+    }
+    
+}
